fcb9e4545193925b554d92ff74f8dede6e6f9aef
MShevyakov <allswell@bk.ru>
2019-11-01 18:02:08 +0300

    upd

--- andEngine/src/main/java/org/andengine/util/modifier/ease/EaseElasticIn.java
+++ andEngine/src/main/java/org/andengine/util/modifier/ease/EaseElasticIn.java
@@ -67,7 +67,7 @@ public class EaseElasticIn implements IEaseFunction {
 		final float s = p / 4;
 
 		final float t = pPercentage - 1;
-		return -(float)Math.pow(2, 10 * t) * FloatMath.sin((t * pDuration - s) * MathConstants.PI_TWICE / p);
+		return -(float)Math.pow(2, 10 * t) * (float) Math.sin((t * pDuration - s) * MathConstants.PI_TWICE / p);
 	}
 
 	// ===========================================================
--- andEngine/src/main/java/org/andengine/util/modifier/ease/EaseSineInOut.java
+++ andEngine/src/main/java/org/andengine/util/modifier/ease/EaseSineInOut.java
@@ -49,7 +49,7 @@ public class EaseSineInOut implements IEaseFunction {
 	public float getPercentage(final float pSecondsElapsed, final float pDuration) {
 		final float percentage = pSecondsElapsed / pDuration;
 
-		return -0.5f * (FloatMath.cos(percentage * PI) - 1);
+		return -0.5f * ((float) Math.cos(percentage * PI) - 1);
 	}
 
 	// ===========================================================
